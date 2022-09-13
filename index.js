11302da9286ca924dffad128856bf9aa27032763
Kcrespo15 <kevincrespo15@gmail.com>
2022-09-13 18:57:11 -0400

    added modules

---
+++ index.js
@@ -0,0 +1,9 @@
+// required odules 
+const inquirer =require('inquirer');
+const fs = require('fs');
+const generateTeam = require('./styling/page-template');
+
+const Manager = require('./Team/Manager');
+const Engineer = require('./Team/Engineer');
+const Inter = require('./Team/Intern');
+
