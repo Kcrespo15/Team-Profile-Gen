a0e13856a2f30240e30b9210ad50d0b663f94304
Kcrespo15 <kevincrespo15@gmail.com>
2022-09-13 20:13:56 -0400

    created index prompt fucntions

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const generateTeam = require('./styling/page-template');
 
 const Manager = require('./Team/Manager');
 const Engineer = require('./Team/Engineer');
-const Inter = require('./Team/Intern');
-const { async } = require('rxjs');
+const Intern = require('./Team/Intern');
+// const { async } = require('rxjs');
 
 const teamMembersData = [];
 
@@ -43,7 +43,89 @@ const questions = async () => {
         }
     ])
 
+    // Prompt selected based on role chosen
+        if (answers.role === 'Manager') {
+            const managerAnswers = await 
+            inquirer.promt ([
+                {
+                    type: "input",
+                    message: "What is your office number",
+                    name: "officeNum",
+                }
+            ])
 
+            const newManager = new Manager(
+                answers.name,
+                answers.id,
+                answers.email,
+                managerAnswers.officeNum
+            );
+            teamMembersData.push(newManager);
+        } 
+            else if (answers.role === 'Engineer') {
+                const gitHubAnswer = await 
+                inquirer.promt([
+                    {
+                        type: "input",
+                        message: "What is your GitHub user name?",
+                        name: "github",
+                    }
+                ])
+                
+                const newEngineer = new Engineer(
+                answers.name,
+                answers.id,
+                answers.email,
+                gitHubAnswer.github
+                );
+                teamMembersData.push(newEngineer);
+          }
+            else if (answers.role === 'Intern') {
+                const internAnswers = await 
+                inquirer.promt([
+                    {
+                        type: "input",
+                        message: "What university did you attend?",
+                        name: "school"
+                    }
+                ])
 
+                const newIntern = new Intern(
+                    answers.name,
+                    answers.id,
+                    answers.email,
+                    internAnswers.school
+                );
+                teamMembersData.push(newIntern);
+            }
 
-} 
+    }; 
+
+    //  create fucntion promt to either create team or add new memeber
+    async function promptQuestions() {
+        await questions()
+          
+        
+        const addMemberAns = await inquirer
+          .prompt([
+            {
+              name:'addMember',
+              type: 'list',
+              choices: ['Add a new member', 'Create team'],
+              message: "What would you like to do next?"
+            }
+          ])
+      
+          if (addMemberAns.addMember === 'Add a new member') {
+            return promptQuestions()
+          }
+          return createTeam();
+      }  
+      
+      promptQuestions();
+
+      // function to create finalized team 
+      function createTeam () {
+        console.log("new member", teamMembersData)
+        fs.writeFileSync("./output/index.html",generateTeam(teamMembersData),"utf-8");
+      }
